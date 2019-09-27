5731501b4228d638c96b5b469a2133d3def5d7b0
Wang Zheng <wz@zlisp.com>
2019-09-27 10:06:00 +0800

    Win10 password tool for ZJF.

---
+++ win10_no_expiration_password.bat
@@ -0,0 +1,45 @@
+@echo off
+
+echo Disable password expiration, then set new password (optional).
+
+REM https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+REM https://www.isumsoft.com/windows-10/how-to-disable-password-expiration-in-windows-10.html
+
+set /p username="Username: "
+wmic UserAccount where Name='%username%' set PasswordExpires=False
+echo Password expiration: disabled.
+
+set /p password="New Password: "
+net user %username% %password%
+
+pause
